d23261b391e9f92fcafa84f118af3c41c812acee
Handi Priyono <handimacbook@gmail.com>
2024-03-26 19:55:39 +0700

    feat: add component list for homepage

---
+++ src/pages/homepage/components/Highlight.tsx
@@ -0,0 +1,24 @@
+const HighLight = () => {
+  return (
+    <>
+      <div className="relative mx-auto flex max-w-3xl flex-col items-center px-6 py-32 text-center sm:py-64 lg:px-0">
+        <h1 className="text-4xl font-bold tracking-tight text-white lg:text-6xl">
+          Special Collection Highlight
+        </h1>
+        <p className="mt-4 text-xl text-white">
+          Sporting a fast-paced look and wavy mudguard with Nike Air cushioning
+          and a Max Air unit, this classic icon hit the scene in '87 and remains
+          the soul of the franchise today.
+        </p>
+        <a
+          href="#"
+          className="mt-8 inline-block rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-900 hover:bg-gray-100"
+        >
+          Shop New Arrivals
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default HighLight;
